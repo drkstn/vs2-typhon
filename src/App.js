c57672d60d3a64c4bf193e2315ad9532d8dfe7a4
drkstn <86159096+drkstn@users.noreply.github.com>
2023-01-29 06:27:20 -0800

    Initial WebMIDI exploration

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { WebMidi } from "webmidi";
+
+WebMidi.enable()
+  .then(onEnabled)
+  .catch((err) => alert(err));
+
+function onEnabled() {
+  // Inputs
+  WebMidi.inputs.forEach((input) =>
+    console.log(input.manufacturer, input.name)
+  );
+
+  // Outputs
+  WebMidi.outputs.forEach((output) =>
+    console.log(output.manufacturer, output.name)
+  );
+
+  const myInput = WebMidi.getInputByName("volca sample");
+  myInput.addListener("noteon", (e) => {
+    console.log(e);
+  });
+
+  const myOutput = WebMidi.getOutputByName("volca sample");
+  let channel = myOutput.channels[1];
+  channel.playNote("C3");
+}
+
+const buttonHandler = () => {
+  WebMidi.enable()
+    .then(onEnabled)
+    .catch((err) => alert(err));
+
+  function onEnabled() {
+    const myOutput = WebMidi.getOutputByName("volca sample");
+    let channel = myOutput.channels[2];
+    // channel.playNote("C3");
+    channel.sendControlChange(7, [127]).playNote("C3");
+    // channel.send([151, 48, 127]);
+  }
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="m-4">
+      <h1 className="font-sans font-bold text-4xl">VS2 Typhon</h1>
+      <button
+        onClick={buttonHandler}
+        className="border-black border p-2 my-2 hover:bg-black hover:text-white"
+      >
+        Test
+      </button>
     </div>
   );
 }
